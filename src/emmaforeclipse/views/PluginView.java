ff48acd722aad121a1aeb08ee5ecbf8929dcc883
yao chen <yaochen1025@gmail.com>
2012-11-07 10:52:14 -0500

    11-07

--- src/emmaforeclipse/views/PluginView.java
+++ src/emmaforeclipse/views/PluginView.java
@@ -5,24 +5,19 @@ import java.io.IOException;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
-
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
 
 
 /**
@@ -50,11 +45,6 @@ public class PluginView extends ViewPart {
 	 */
 	public static final String ID = "emmaforeclipse.views.PluginView";
 
-	private TableViewer viewer;
-	private Action action1;
-	private Action action2;
-	private Action doubleClickAction;
-
 	private Text projectDir;
 	private Text testDir;
 	private Button projectDirSelectButton;
@@ -63,8 +53,6 @@ public class PluginView extends ViewPart {
 	private Text command;
 	private Button runButton;
 
-	Composite parent;
-	
 
 	/**
 	 * This is a callback that will allow us
@@ -72,8 +60,8 @@ public class PluginView extends ViewPart {
 	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		this.parent = parent;
-		
+
+
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 3;
 		parent.setLayout(layout);
@@ -85,7 +73,7 @@ public class PluginView extends ViewPart {
 		projectDirSelectButton.setText("Choose");
 		projectDir.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		projectDirSelectButton.setLayoutData(new GridData(GridData.END, SWT.BEGINNING, true, false));
-		addFileChooserListener(projectDirSelectButton, projectDir);
+		addFileChooserListener(parent, projectDirSelectButton, projectDir);
 
 		new Label(parent, SWT.NONE).setText("Test Directory");
 		testDir = new Text(parent, SWT.BORDER);
@@ -93,9 +81,9 @@ public class PluginView extends ViewPart {
 		testDirSelectButton.setText("Choose");
 		testDir.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		testDirSelectButton.setLayoutData(new GridData(GridData.END, SWT.BEGINNING, true, false)); 
-		addFileChooserListener(testDirSelectButton, testDir);
-		
-		
+		addFileChooserListener(parent, testDirSelectButton, testDir);
+
+
 		new Label(parent, SWT.NONE).setText("Command");
 		command = new Text(parent, SWT.BORDER);
 		runButton = new Button(parent, SWT.PUSH);
@@ -103,7 +91,7 @@ public class PluginView extends ViewPart {
 		command.setText("open -t /Users/glcylily/Documents/workspace/EmmaForEclipse/plugin.xml");
 		command.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		runButton.setLayoutData(new GridData(GridData.END, SWT.BEGINNING, true, false)); 
-		
+
 		runButton.addListener(SWT.Selection, new Listener() {
 
 			@Override
@@ -118,162 +106,56 @@ public class PluginView extends ViewPart {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 		});
-		
+
 	}
 
-	private void addFileChooserListener(Button button, Text text) {
-		
+	private void addFileChooserListener(Composite parent, Button button, Text text) {
+
 		FileSelectListener l = new FileSelectListener();
 		l.text = text;
+		l.shell = parent.getShell();
 		button.addListener(SWT.Selection, l);
 	}
-//	
+
 	class FileSelectListener implements Listener{
 		Text text;
+		Shell shell;
 		@Override
-		public void handleEvent(Event arg0) {			
-			DirectoryDialog directoryDialog = new DirectoryDialog(parent.getShell());
-			directoryDialog.setFilterPath("/Users/glcylily");
-			directoryDialog.setMessage("Please select a directory and click OK");
-
-			String dir = directoryDialog.open();
-			if(dir != null) {
-				text.setText(dir);
-			}			
+		public void handleEvent(Event arg0) {		
+			
+			ContainerSelectionDialog dialog = new ContainerSelectionDialog(shell, null, true, "Select a directory");
+			dialog.setTitle("Container Selection");
+			dialog.open();
+			Object[] result = dialog.getResult();
+			for(Object o : result) {
+				text.setText(o.toString());
+			}
+		
+			//			DirectoryDialog directoryDialog = new DirectoryDialog(shell, SWT.NULL);
+//			directoryDialog.setFilterPath("/Users/glcylily/Documents/workspace/");
+//			directoryDialog.setMessage("Please select a directory and click OK");
+//			String dir = directoryDialog.open();
+//			if(dir != null) {
+//				text.setText(dir);
+//			}		
+//			Display display = shell.getDisplay();
+//			while (!shell.isDisposed()) {
+//				if (!display.readAndDispatch())
+//					display.sleep();
+//			}
+//			display.dispose();
 		}	
 	}
-	
-	
 
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
-	 */
-
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-		public void dispose() {
-		}
-		public Object[] getElements(Object parent) {
-			return new String[] { "One", "Two", "Three" };
-		}
-	}
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
-	class NameSorter extends ViewerSorter {
-	}
-
-	/**
-	 * The constructor.
-	 */
-	public PluginView() {
-	}
-
-
-
-	private void hookContextMenu() {
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(IMenuManager manager) {
-				PluginView.this.fillContextMenu(manager);
-			}
-		});
-		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(menuMgr, viewer);
-	}
-
-	private void contributeToActionBars() {
-		IActionBars bars = getViewSite().getActionBars();
-		fillLocalPullDown(bars.getMenuManager());
-		fillLocalToolBar(bars.getToolBarManager());
-	}
-
-	private void fillLocalPullDown(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(new Separator());
-		manager.add(action2);
-	}
-
-	private void fillContextMenu(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(action2);
-		// Other plug-ins can contribute there actions here
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-	}
-
-	private void fillLocalToolBar(IToolBarManager manager) {
-		manager.add(action1);
-		manager.add(action2);
-	}
-
-	private void makeActions() {
-		action1 = new Action() {
-			public void run() {
-				showMessage("Action 1 executed");
-			}
-		};
-		action1.setText("Action 1");
-		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-
-		action2 = new Action() {
-			public void run() {
-				showMessage("Action 2 executed");
-			}
-		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		doubleClickAction = new Action() {
-			public void run() {
-				ISelection selection = viewer.getSelection();
-				Object obj = ((IStructuredSelection)selection).getFirstElement();
-				showMessage("Double-click detected on "+obj.toString());
-			}
-		};
-	}
-
-	private void hookDoubleClickAction() {
-		viewer.addDoubleClickListener(new IDoubleClickListener() {
-			public void doubleClick(DoubleClickEvent event) {
-				doubleClickAction.run();
-			}
-		});
-	}
-	private void showMessage(String message) {
-		MessageDialog.openInformation(
-				viewer.getControl().getShell(),
-				"Sample View",
-				message);
-	}
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
+	@Override
 	public void setFocus() {
 		//viewer.getControl().setFocus();
 	}
